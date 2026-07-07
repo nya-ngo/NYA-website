298af8545cd864172579e7f125a4326ef7d5870c
Tasila Hasini <tasilahasini@gmail.com>
2026-07-07 11:17:16 +0530

    Revert "added contact page"

--- root/backend/src/middleware/errorHandler.js
+++
@@ -1,8 +0,0 @@
-export default function errorHandler(err, req, res, next) {
-  console.error(err);
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-  res.status(statusCode).json({
-    message: err.message || "Internal Server Error",
-    stack: process.env.NODE_ENV === "production" ? undefined : err.stack,
-  });
-}
--- root/frontend/app/loading.tsx
+++
@@ -1,5 +0,0 @@
-export default function loading(){
-    <>
-    <div className="bg-black text-white">loading ....</div>
-    </>
-}
